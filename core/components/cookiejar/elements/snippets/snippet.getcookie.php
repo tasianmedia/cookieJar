4811b1c612f430768f9ca0f875b1d5cd0428d338
David Pede <davidpede@users.noreply.github.com>
2018-12-12 12:01:31 +0000

    Improve error handling

--- core/components/cookiejar/elements/snippets/snippet.getcookie.php
+++ core/components/cookiejar/elements/snippets/snippet.getcookie.php
@@ -2,14 +2,14 @@
 /**
  * A Cookie toolkit Extra for MODX Revolution.
  *
- * @author David Pede <dev@tasian.media> <https://twitter.com/davepede>
+ * @author David Pede <dev@tasian.media>
  * @version 1.0.3-pl
  * @released September 29, 2016
  * @since April 23, 2014
  * @package cookiejar
  * @snippet getcookie
  *
- * Copyright (C) 2016 David Pede. All rights reserved. <dev@tasian.media>
+ * Copyright (C) 2018 Tasian Media. All rights reserved. <studio@tasian.media>
  *
  * cookieJar is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
@@ -22,6 +22,9 @@
  * You should have received a copy of the GNU General Public License along with
  * cookieJar; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @var modX $modx
+ * @var array $scriptProperties
  */
 
 /* set default properties */
@@ -31,23 +34,21 @@ $toPlaceholder = !empty($toPlaceholder) ? $toPlaceholder : '';
 
 $output = '';
 
-if(isset($name) && isset($_COOKIE[$name])) {
-  $cookie = htmlspecialchars($_COOKIE[$name]);
-  if(empty($tpl) && $toPlaceholder === 'value') {
-    $result = $cookie;
-  }else{
-    if (!empty($toPlaceholder)) {
-      $modx->setPlaceholder($toPlaceholder,$cookie); 
+if (isset($name) && isset($_COOKIE[$name])) {
+    $cookie = htmlspecialchars($_COOKIE[$name]);
+    if (empty($tpl) && $toPlaceholder === 'value') {
+        $output = $cookie;
+    } else {
+        if (!empty($toPlaceholder)) {
+            $modx->setPlaceholder($toPlaceholder, $cookie);
+        }
+        if (!empty($tpl)) {
+            $ph = $modx->setPlaceholder($toPlaceholder, $cookie);
+            $output = $modx->getChunk($tpl);
+        }
     }
-    if (!empty($tpl)) {
-      $ph = $modx->setPlaceholder($toPlaceholder,$cookie);
-      $result = $modx->getChunk($tpl);
-    }
-  }
-}else{
-  $modx->log(xPDO::LOG_LEVEL_DEBUG, 'getCookie() - Cookie name not recognised.');
+} else {
+    $modx->log(xPDO::LOG_LEVEL_ERROR, "[[getCookie]] - Cookie with name '{$name}' not recognised.");
 }
-
-$output = $result;
 
 return $output;
